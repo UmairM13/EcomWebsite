06b99dd1d39941b33d0a7077490349a0f7fdbd07
UmairM13 <22493168@stu.mmu.ac.uk>
2025-01-29 19:10:36 +0000

    initial commit #2

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const net = require("net");
+const WebSocket = require("ws");
+
+const app = express();
+app.use(cors());
+
+// Server port
+const HTTP_PORT = 3333;
+const TCP_PORT = 8080;
+
+// Logging
+app.use(morgan("tiny"));
+
+// Body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Root endpoint
+app.get("/", (req, res, next) => {
+  res.json({ status: "Alive" });
+});
+
+// Other API endpoints: Links go here...
+require("./app/routes/user.server.routes.js")(app);
+require("./app/routes/food.server.routes.js")(app);
+require("./app/routes/bmi.server.routes")(app);
+
+// Create HTTP server
+const server = app.listen(HTTP_PORT, () => {
+  console.log("HTTP server running on port: " + HTTP_PORT);
+});
